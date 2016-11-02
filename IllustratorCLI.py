aa34e39213cfeb206f062ac7d73990f83becca10
salchoman <salchoman@gmail.com>
2016-11-02 21:05:11 +0000

    Basic CLI

---
+++ IllustratorCLI.py
@@ -0,0 +1,34 @@
+from Adafruit_MotorHAT import *
+from illustrator.Illustrator import *
+import threading
+
+hat = Adafruit_MotorHAT()
+illustrator = Illustrator(hat, 30, 30, (0, 0), (50,50))
+illustrator.start()
+
+orig = exit
+def exit():
+	print '\n[+] Motors off...'
+	illustrator._turnOff()
+	orig()
+
+try:
+	while True:
+		x = -1
+		y = -1
+		try:
+			x = raw_input('x ----> ')
+			if x == 'exit':
+				exit()	
+			x = int(x)
+			y = int(raw_input('y ----> '))
+		except ValueError:
+			print "\n\n\n"
+			continue
+		
+		if x == -1 or y == -1:
+			continue
+
+		illustrator.go(x, y)
+except KeyboardInterrupt:
+	exit()	
