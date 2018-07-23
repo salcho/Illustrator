774aefef5357b34160db220273f6f20dce2e5f89
Santiago Díaz <salchoman@gmail.com>
2018-07-23 19:31:54 +0100

    allow CLI to set steps/cm and steps/mm

--- IllustratorCLI.py
+++ IllustratorCLI.py
@@ -1,11 +1,10 @@
 from Adafruit_MotorHAT import *
 
 from illustrator.CartesianIllustrator import CartesianIllustrator
-from illustrator.Illustrator import *
-import threading
+from illustrator.Engine import Engine
 
 hat = Adafruit_MotorHAT()
-illustrator = CartesianIllustrator(hat, canvasDimensions=(30, 30), initialPositions=(0, 0), belt_lengths=(50,50))
+illustrator = CartesianIllustrator(hat, canvasDimensions=(30, 30), initialPositions=(0, 0), belt_lengths=(50, 50))
 print '------------------'
 print 'Canvas dimensions: (30, 30)'
 print 'Initial positions: (0, 0)'
@@ -14,29 +13,41 @@ print '------------------'
 illustrator.start()
 
 orig = exit
+
+
 def exit():
-	print '\n[+] Motors off...'
-	illustrator._turnOff()
-	orig()
+    print '\n[+] Motors off...'
+    illustrator._turnOff()
+    orig()
+
 
 try:
-	while True:
-		x = -1
-		y = -1
-		try:
-			x = raw_input('x ----> ')
-			if x == 'exit':
-				exit()	
-			x = int(x)
-			y = int(raw_input('y ----> '))
-		except ValueError:
-			print "\n\n\n"
-			continue
-		
-		if x == -1 or y == -1:
-			continue
+    while True:
+        x = -1
+        y = -1
+        try:
+            x = raw_input('x ----> ')
+            if x == 'exit':
+                exit()
+            elif x == 'setcm':
+                val = raw_input('steps per cm ----> ')
+                Engine.STEPS_PER_CM = val
+                continue
+            elif x == 'setmm':
+                val = raw_input('steps per cm ----> ')
+                Engine.STEPS_PER_MM = val
+                continue
 
-		illustrator.go(x, y)
-		illustrator.join()
+            x = int(x)
+            y = int(raw_input('y ----> '))
+        except ValueError:
+            print "\n\n\n"
+            continue
+
+        if x == -1 or y == -1:
+            continue
+
+        illustrator.go(x, y)
+        illustrator.join()
 except KeyboardInterrupt:
-	exit()	
+    exit()
